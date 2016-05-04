d8a96281d89f4b731e38d2d45f9eead21d320baa
Impellettieri Florian <florian.impellettieri@etu.utc.fr>
2016-05-04 13:21:07 +0200

    change shared project to portable project

--- Colladia/Colladia/Colladia.Droid/MainActivity.cs
+++ Colladia/Colladia/Colladia.Droid/MainActivity.cs
@@ -9,16 +9,16 @@ using Android.OS;
 
 namespace Colladia.Droid
 {
-	[Activity (Label = "Colladia", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+    [Activity(Label = "Colladia", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
-			LoadApplication (new Colladia.App ());
-		}
-	}
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
 }
 
